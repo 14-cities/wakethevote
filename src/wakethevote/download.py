5a95bbf10ec06b5c7bd5f06b64f80db297fb486a
Josh West <18324+joshmaker@users.noreply.github.com>
2020-01-03 16:34:20 -0800

    Tweak the download_county func to log exceptions
    instead of raising them

--- src/wakethevote/download.py
+++ src/wakethevote/download.py
@@ -1,13 +1,23 @@
+import requests
+
 from .census import load_census_block_data
 from .logger import logger
 from .org_units import get_org_units
 from .paths import get_county_data_path
+from .types import County
 
 
-def download_county(county) -> None:
+def download_county(county: County) -> None:
+    logger.info(
+        f"*** Downloading data for {county.name} {county.state} ({county.fips}) ***"
+    )
 
     logger.info("Loading Census block data")
-    blocks = load_census_block_data(county)
+    try:
+        blocks = load_census_block_data(county)
+    except requests.exceptions.RequestException as e:
+        logger.warning(e)
+        return
 
     logger.info("Clustering into org units")
     blocks = get_org_units(blocks)
@@ -16,40 +26,46 @@ def download_county(county) -> None:
     county_path = get_county_data_path(county)
     org_units_shapefile_name = county_path / f"{county.name}_orgunits.shp"
     org_units_csv_name = county_path / f"{county.name}_orgunits.csv"
-    blocks.to_file(org_units_shapefile_name)
-    blocks.drop(["geometry"], axis=1).to_csv(org_units_csv_name, index=False)
 
-    # write metdatada
-    with open(county_path / "README.txt", "w") as meta:
-        meta.write(
-            """Organizational Voting Units.
-    These are Census blocks that are majority black and have at least 50 black households (BHH).
-    Adjacent census blocks with fewer than 50 BHH are aggregated together until 100 BHH are found.
-
-    Data dictionary:
-        'RandomID' - Randomized org unit ID
-        'OrgType' - Org type (block or block aggregate)
-        'Precinct' - Precinct number
-        'BlackHH' - Estimated Black HH
-        'Total_census_population' - Total census population
-        'Total_census_Black_population' - Total census Black population
-        'Pct_Black_census' - --% Black pop (census)
-        'Total_Black_registered_population' - Total Black registered population
-        'square_miles' - Area of unit in square miles
-        'MECE1' - # of black voters in MECE1
-        'MECE2' - # of black voters in MECE2
-        'MECE3' - # of black voters in MECE3
-        'MECE4' - # of black voters in MECE4
-        'MECE5' - # of black voters in MECE5
-        'city' - City in which majority of org unit is found
-        'support_volunteer_name' -
-        'support_vol_phone' -
-        'support_vol_email' -
-        'block_team_member' -
-        'block_team-phone', -
-        'block_team_email' -
-        'Notes'  -
-
-        """
+    if blocks.empty:
+        logger.warning(
+            f"No org units in GeoDataFrame for {county.name} {county.state} ({county.fips}"
         )
-    logger.info("    Org units saved to {orgunits_shapefile_filename}")
+    else:
+        blocks.to_file(org_units_shapefile_name)
+        blocks.drop(["geometry"], axis=1).to_csv(org_units_csv_name, index=False)
+
+        # write metdatada
+        with open(county_path / "README.txt", "w") as meta:
+            meta.write(
+                """Organizational Voting Units.
+        These are Census blocks that are majority black and have at least 50 black households (BHH).
+        Adjacent census blocks with fewer than 50 BHH are aggregated together until 100 BHH are found.
+
+        Data dictionary:
+            'RandomID' - Randomized org unit ID
+            'OrgType' - Org type (block or block aggregate)
+            'Precinct' - Precinct number
+            'BlackHH' - Estimated Black HH
+            'Total_census_population' - Total census population
+            'Total_census_Black_population' - Total census Black population
+            'Pct_Black_census' - --% Black pop (census)
+            'Total_Black_registered_population' - Total Black registered population
+            'square_miles' - Area of unit in square miles
+            'MECE1' - # of black voters in MECE1
+            'MECE2' - # of black voters in MECE2
+            'MECE3' - # of black voters in MECE3
+            'MECE4' - # of black voters in MECE4
+            'MECE5' - # of black voters in MECE5
+            'city' - City in which majority of org unit is found
+            'support_volunteer_name' -
+            'support_vol_phone' -
+            'support_vol_email' -
+            'block_team_member' -
+            'block_team-phone', -
+            'block_team_email' -
+            'Notes'  -
+
+            """
+            )
+        logger.info(f"    Org units saved to {org_units_shapefile_name}")
